7faa6574d1128b8b272e0850651a730ece82da3e
suleman <iam.suleman.m@gmail.com>
2023-06-14 16:28:20 +0500

    Creating login and register page and styling these pages.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Product from './pages/Product';
+// import Product from './pages/Product';
 // import Home from './pages/Home';
 // import ProductList from './pages/ProductList';
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <Product />
+      {/* <Register /> */}
+      <Login />
+      {/* <Product /> */}
+      {/* <ProductList /> */}
       {/* <Home /> */}
     </div>
   );
