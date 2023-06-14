55d997170850b7e71730f927f01261d2a096c2c4
suleman <iam.suleman.m@gmail.com>
2023-06-14 22:11:52 +0500

    Creating cart page and styling this page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 // import Product from './pages/Product';
 // import Home from './pages/Home';
 // import ProductList from './pages/ProductList';
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 // import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div>
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Product /> */}
       {/* <ProductList /> */}
       {/* <Home /> */}
+      <Cart />
     </div>
   );
 }
